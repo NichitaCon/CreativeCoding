eae9492840e8c002e184c2d56aa8bbc010dac7b9
Nichita Condrea (Student) <N00233189@iadt.ie>
2025-02-18 15:24:43 +0000

    Cleanup

--- playgrounds/Playground Sample FIles/Playground 05 - PieChart/sketch.js
+++ playgrounds/Playground Sample FIles/Playground 05 - PieChart/sketch.js
@@ -5,8 +5,8 @@ let chartWidth = 400;
 let barWidth = 30;
 let margin = 15;
 let axisThickness = 1;
-let chartPosX = 50;
-let chartPosY = 350;
+let chartPosX = 200;
+let chartPosY = 200;
 let yValue = "Female";
 let xValue = "Age_Group";
 let gap;
@@ -15,8 +15,13 @@ let axisColour;
 let barColour;
 let axisTextColour;
 
+let myNewArray;
+let total;
+let font;
+
 function preload() {
 	data = loadTable("data/Combined.csv", "csv", "header");
+	font = loadFont("static/Montserrat-Medium.ttf")
 }
 
 function setup() {
@@ -33,6 +38,12 @@ function setup() {
 	axisColour = color(100);
 	barColour = color(0, 200, 50);
 	axisTextColour = color(125);
+
+	myNewArray = cleanedData.map(row => row.Female);
+	total = 0;
+	myNewArray.forEach(item => total = total + item);
+	console.log(total);
+
 }
 
 function draw() {
@@ -41,9 +52,27 @@ function draw() {
 	push();
 	translate(chartPosX, chartPosY);
 
+	
 
-
+	for(let i = 0; i<myNewArray.length; i++){
+		fill(random(255), 200, random(0))
+		stroke(255);
+		let start = 0;
+		let end = ((myNewArray[i]/total)*360);
+		arc (0,0, 200, 200, start, end,PIE)
+		rotate(end);
+	}
+	textSize(30)
+	fill(0)
+	noStroke()
+	textFont(font)
+	text("NICHITAAA",0,0)
 	pop();
+
+	
+	
+
+
 }
 
 function cleanData() {
